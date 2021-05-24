d31a4283f092378fcee35d4da61eb159395a0bd7
Creuma Kuzola <creumakuzola@gmail.com>
2021-05-24 10:46:56 +0100

    Teste

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  TInfo
+{
+    int chave;
+    int valor;
+}TInfo;
+
+
+
+typedef struct TAtomo
+{
+    TInfo info;
+    
+}TAtomo;
+
+
+
+int main (){
+
+    return 0;
+}
