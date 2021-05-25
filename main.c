a11bd12d1c8f4f59a2f317b46f7267b33abb8b0e
Creuma Kuzola <creumakuzola@gmail.com>
2021-05-25 14:43:03 +0100

    Funcoes inserir, imprimir, vazia e listaUnitaria feitas

--- main.c
+++ main.c
@@ -1,23 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 80
+#define NOT_FOUND -1 // Item não existe
+#define OK 0 // Operação realizada com sucesso
+#define LIST_EMPTY 2 // Lista vazia
+#define NO_SPACE 3 // Não há espaço de memória
 
-typedef struct  TInfo
+typedef enum Boolean{FALSE = 0, TRUE = 1}Boolean;
+
+typedef struct TInfo
 {
-    int chave;
-    int valor;
+    char frase[MAX];
+    Boolean linhaCorrente;
+    
 }TInfo;
-
 
 
 typedef struct TAtomo
 {
     TInfo info;
+    struct TAtomo *eprox;
+    struct TAtomo *dprox;
     
 }TAtomo;
 
+typedef struct TDLEnc{
+
+    TAtomo *primeiro;
+
+}TDLEnc;
+
+void criarLista(TDLEnc *lista){
+
+    lista->primeiro = NULL;
+}
+
+Boolean vaziaLista(TDLEnc *lista){
+
+    return (lista->primeiro == NULL);
+}
+
+void imprimirLista(TDLEnc *lista){
+
+    for(TAtomo *paux = lista->primeiro; paux->dprox != lista->primeiro; paux = paux ->dprox){
+        printf("%s\n", paux->info.frase);
+    }
+    printf("\n");
+}
+
+Boolean listaUnitaria(TDLEnc *lista){
+
+    return (lista->primeiro != NULL && lista->primeiro->dprox == NULL);
+}
+
+int inserirElemento(TDLEnc *lista, TInfo info){
+
+    TAtomo *pnovo = (TAtomo*) malloc(sizeof(TAtomo));
+    if(pnovo == NULL) return NO_SPACE;
+    pnovo->info = info;
+    if(vaziaLista(lista)){
+        lista->primeiro = pnovo;
+    }
+    if(listaUnitaria(lista)){
+        lista->primeiro->dprox = pnovo;
+    }
+    else{
+
+        pnovo->eprox = lista->primeiro->eprox;
+        pnovo->dprox = lista->primeiro;
+    }
+
+    lista->primeiro->eprox = pnovo;
+    return OK;
+}
 
 
 int main (){
 
-    return 0;
+
+
 }
